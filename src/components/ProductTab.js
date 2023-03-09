792a6d8382f52572fe26dbeb60511e4c209540c4
divyang.thinktanker <divyang.thinktanker@gmail.com>
2023-03-09 19:01:57 +0530

    date 09-03-2023

--- src/components/ProductTab.js
+++ src/components/ProductTab.js
@@ -9,6 +9,8 @@ const ProductTab = () => {
   const [seoMetafields, setSeoMetafields] = useState(false);
   const [hsCode, setHsCode] = useState(false);
   const [media, setMedia] = useState(false);
+  const [store, setStore] = useState(false);
+  const [hideSynced, setHideSynced] = useState(false);
   return (
     <div className="grid">
       <div className="col-12">
@@ -43,6 +45,14 @@ const ProductTab = () => {
               <div className='border-1 border-gray-400 border-round-xl flex flex-column md:flex-row  md:align-items-center mb-3 p-3'>
                 <InputSwitch checked={media} onChange={(e) => setMedia(e.value)} />
                 <h5 className='my-0 ml-4 font-bold text-lg text-color-secondary'>Media-Images &#123; BETA &#125;</h5>
+              </div>
+              <div className='border-1 border-gray-400 border-round-xl flex flex-column md:flex-row  md:align-items-center mb-3 p-3'>
+                <InputSwitch checked={store} onChange={(e) => setStore(e.value)} />
+                <h5 className='my-0 ml-4 font-bold text-lg text-color-secondary'>Online Store Sales Channel</h5>
+              </div>
+              <div className='border-1 border-gray-400 border-round-xl flex flex-column md:flex-row  md:align-items-center mb-3 p-3'>
+                <InputSwitch checked={hideSynced} onChange={(e) => setHideSynced(e.value)} />
+                <h5 className='my-0 ml-4 font-bold text-lg text-color-secondary'>Hide Synced Product By Default</h5>
               </div>
             </div>
             <div className='col-12 md:col-7 surface-100 ml-4 my-4' style={{ width: "64%" }}>
